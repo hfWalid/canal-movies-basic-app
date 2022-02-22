a7a48f54109a1cb1ca62d440996b18115a970194
walidHefhaf <whefhaf@alter-solutions.com>
2022-02-22 16:59:13 +0000

    unnecessary distiction removed

--- src/utils/httpRequest/endpoints.js
+++ src/utils/httpRequest/endpoints.js
@@ -1,6 +1,5 @@
-import {BASE_URL, NEXT_PUBLIC_API_KEY} from "./api-service";
 
 export const Endpoints = {
-    moviesList: (page) => `trending/all/day?api_key=${NEXT_PUBLIC_API_KEY}&page=${page}`,
+    moviesList: (page) => `&page=${page}`,
 
 }
